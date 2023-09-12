9b589fd9900ab650e7ed02da4f5e25c0485a4f16
신근재 <60930743+Boknami@users.noreply.github.com>
2023-09-12 20:10:35 +0900

    [Solved] 1260_DFS_BFS

--- DFS_BFS/24479_dfs.java
+++ DFS_BFS/24479_dfs.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
@@ -7,53 +8,42 @@ public class Main {
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
     public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int R = sc.nextInt();
         visited = new int[N+1];
         cnt = 1;
 
-        //정점 숫자만큼 넓혀준다.
-        for(int i =0; i < N+1; i++) {
+        for(int i = 0 ; i < N+1; i++){
             graph.add(new ArrayList<>());
         }
 
-        //그래프에 정보 입력
-        for(int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start_dot = Integer.parseInt(st.nextToken());
-            int end_dot = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < M; i++){
+            int startNode = sc.nextInt();
+            int endNode = sc.nextInt();
 
-            // 1->4 && 4->1
-            graph.get(start_dot).add(end_dot);
-            graph.get(end_dot).add(start_dot);
+            graph.get(startNode).add(endNode);
+            graph.get(endNode).add(startNode);
         }
 
-        // 오름차순을 위해 정렬
-        for(int i = 1; i < graph.size(); i++) {
-            Collections.sort(graph.get(i));
+        for(int i = 1 ; i < N+1; i++){
+            Collections.sort(graph.get(i), Collections.reverseOrder());
         }
 
         dfs(R);
+
         for(int i = 1 ; i < N+1; i++){
             System.out.println(visited[i]);
         }
     }
 
-    public static void dfs(int R) {  // V : 정점 집합, E : 간선 집합, R : 시작 정점
-        visited[R] = cnt;
-
-        //인접한 정점들을 어떻게 뽑아낼래?
-        for(int i = 0 ; i  < graph.get(R).size();  i++){
-            int newVertex = graph.get(R).get(i);
-            if(visited[newVertex] == 0){
+    public static void dfs(int R) {
+        visited[R] = cnt;//방문 표시
+        for(int i = 0; i < graph.get(R).size(); i++){//현재 노드에서 방문할 수 있는 것들 전~부
+            if(visited[graph.get(R).get(i)]== 0){
                 cnt++;
-                dfs(newVertex);
+                dfs(graph.get(R).get(i));
             }
         }
     }
