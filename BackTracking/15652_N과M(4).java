16526c30207e9d5a3135f08a8f859cf691fcae40
신근재 <60930743+Boknami@users.noreply.github.com>
2023-09-15 23:14:06 +0900

    [Solved] 15652_N과M(4)

---
+++ BackTracking/15652_N과M(4).java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int num;
+    static int selectNum;
+    static int[] ary;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        num = Integer.parseInt(st.nextToken());
+        selectNum = Integer.parseInt(st.nextToken());
+        ary = new int[selectNum];
+
+        DFS(0);
+        System.out.print(sb);
+    }
+
+    private static void DFS(int depth) {//원하는 깊이까지 만들어졌는지 확인해야지
+        if(depth == selectNum){
+            for (int i = 0; i < selectNum; i++)
+                sb.append(ary[i] + " ");
+            sb.append('\n');
+            return;
+        }
+
+        //차례 차례 방문한다.
+        for(int i = 0; i < num; i++){
+            if(depth > 0 && ary[depth-1] > i+1)
+                continue;
+            ary[depth] = i+1;//ary에 넣자
+            DFS(depth+1);
+        }
+    }
+}
